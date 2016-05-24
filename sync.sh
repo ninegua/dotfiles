e5e56f56d2e639ecdb8c04056433760e609ccf79
Paul H. Liu <paul@thev.net>
2016-05-24 10:18:28 -0700

    Add screenrc

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-for item in Xdefaults vimrc bashrc dircolors.ansi-dark vim/ fonts/ config/; do
+for item in screenrc Xdefaults vimrc bashrc dircolors.ansi-dark vim/ fonts/ config/; do
 echo Sync $item
 rsync -a $item $HOME/.$item
 done
